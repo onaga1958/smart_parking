43c7685f0190468c3e33a4abdc61fe05737d6972
Alex Zabelin <onaga1958@yandex.ru>
2019-09-28 11:36:19 +0200

    some dummy logic

--- backend/smart_parking/views.py
+++ backend/smart_parking/views.py
@@ -6,6 +6,14 @@ from django.http import JsonResponse
 
 from keys import GOOGLE_KEY
 
+parkings = {
+    "City Parking": "Gessnerallee 14, 8001 Zürich",
+    "Jelmoli": "Steinmühlepl. 1, 8001 Zürich",
+    "Globus": "Schweizergasse 11, 8001 Zürich",
+    "Urania": "Uraniastrasse 3, 8001 Zürich",
+    "Talgarten": "Nüschelerstrasse 31, 8001 Zürich",
+}
+
 
 def get_distanse(origin, destination):
     url = (
@@ -14,6 +22,10 @@ def get_distanse(origin, destination):
     )
     data = requests.get(url)
     return data["rows"][0]["elements"][0]["duration"]["value"]
+
+
+def get_model_prediction(parking_name, time):
+    return 0.5
 
 
 class APIEndpoint(View):
@@ -28,4 +40,10 @@ class APIEndpoint(View):
 
 class FindParkingsEndpoint(APIEndpoint):
     def _do(self, adress=None, time=None):
-        return {'a': adress, 'b': time}
+        distance = {}
+        for name, parking_adress in parkings.items():
+            distance[name] = get_distanse(adress, parking_adress)
+        for name, distance in sorted(distance.items(), key=lambda x: x[1]):
+            occupation = get_model_prediction(name, time)
+            if occupation < 0.8:
+                return {"adress": parkings[name], "occupation": occupation}
